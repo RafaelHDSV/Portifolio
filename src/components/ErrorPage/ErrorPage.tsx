011bc216c9bc07a3fc893efd21b02ea2742a0d53
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-03-30 17:06:29 -0300

    fix: update click handling for portfolio text in DevelopmentScreen component

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -8,12 +8,12 @@ export default function DevelopmentScreen({ clickCount }: IDevelopmentScreenProp
   return (
     <main className={styles.main}>
       <div className={styles.errorPage}>
-        <img className={styles.leftAstronaut} src='./astronaut.png' alt='astronaut.png' onClick={() => clickCount(prev => prev + 1)} />
+        <img className={styles.leftAstronaut} src='./astronaut.png' alt='astronaut.png' />
         <img className={styles.rightAstronaut} src='./astronaut2.svg' alt='astronaut2.svg' />
 
         <div className={styles.errorText}>
           <h1>
-            Obrigado por visitar meu <b>portfólio</b>!
+            Obrigado por visitar meu <b onClick={() => clickCount(prev => prev + 1)}>portfólio</b>!
           </h1>
           <p>Este projeto ainda está em desenvolvimento. Fique atento para novidades em breve!</p>
         </div>
