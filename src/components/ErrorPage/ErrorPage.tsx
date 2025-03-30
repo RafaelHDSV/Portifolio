4db0d08778f03c61029fb1efb8b984b2085ffa99
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-03-30 16:38:17 -0300

    feat: implement ErrorPage component with astronaut visuals and background effects

---
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,30 @@
+import styles from './ErrorPage.module.scss'
+
+interface IDevelopmentScreenProps {
+  clickCount: (value: (prev: number) => number) => void
+}
+
+export default function DevelopmentScreen({ clickCount }: IDevelopmentScreenProps) {
+  return (
+    <>
+      <div className={styles.errorPage}>
+        <img className={styles.leftAstronaut} src='./astronaut.png' alt='astronaut.png' onClick={() => clickCount(prev => prev + 1)} />
+        <img className={styles.rightAstronaut} src='./astronaut2.svg' alt='astronaut2.svg' />
+
+        <div className={styles.errorText}>
+          <h1>
+            Obrigado por visitar meu <b>portfólio</b>!
+          </h1>
+          <p>Este projeto ainda está em desenvolvimento. Fique atento para novidades em breve!</p>
+        </div>
+      </div>
+
+      <div className={styles.bg}></div>
+      <div className={styles.starField}>
+        <div className={styles.layer}></div>
+        <div className={styles.layer}></div>
+        <div className={styles.layer}></div>
+      </div>
+    </>
+  )
+}
