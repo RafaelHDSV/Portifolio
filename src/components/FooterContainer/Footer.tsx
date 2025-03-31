621d16bacbea54fad8cc04d1844d6c608b11140a
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-03-31 03:46:58 -0300

    fix: update copyright year to 2025 in LICENSE and Footer component

--- src/components/FooterContainer/Footer.tsx
+++ src/components/FooterContainer/Footer.tsx
@@ -1,5 +1,5 @@
 // import styles from './Footer.module.scss'
 
 export const FooterContainer = () => {
-  return <footer>Copyright &copy; 2024 | Rafael Vieira. Todos os direitos reservados.</footer>
+  return <footer>Copyright &copy; 2025 | Rafael Vieira. Todos os direitos reservados.</footer>
 }
