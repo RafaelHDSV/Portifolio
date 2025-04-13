17de7d2d75fb7bd7099584fd89c8ea71b26dd872
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-04-13 18:00:00 -0300

    feat: refactor useGetMe and add useGetRepos for improved GitHub API integration

--- src/hooks/useGithubApi.ts
+++ src/hooks/useGithubApi.ts
@@ -1,23 +1,25 @@
 import { useEffect, useState } from 'react'
-import { githubApi } from '../services/apis/gitHub'
-import { IGithubUser } from '../interfaces'
+import { IGithubRepo, IGithubUser } from '../interfaces'
+import { GithubRepository } from '../repository/GithubRepository'
 
 export function useGetMe() {
   const [user, setUser] = useState<IGithubUser>()
   const [loading, setLoading] = useState(false)
-  const [error, setError] = useState<string>()
+  const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
     const getUserApi = async () => {
       try {
         setLoading(true)
-        const response = await githubApi.get('/users/RafaelHDSV')
-        setUser(response.data)
+
+        const user = await GithubRepository.getUserDetails('RafaelHDSV')
+        setUser(user)
       } catch (err) {
         console.error('Error fetching user from GitHub:', err)
         setError(err as string)
       } finally {
         setLoading(false)
+        setError(null)
       }
     }
 
@@ -26,6 +28,41 @@ export function useGetMe() {
 
   return {
     user,
+    loading,
+    error
+  }
+}
+
+export function useGetRepos() {
+  const [repos, setRepos] = useState<IGithubRepo[]>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    const getReposApi = async () => {
+      try {
+        setLoading(true)
+        const params = {
+          sort: 'updated',
+          per_page: '100'
+        }
+
+        const repos = await GithubRepository.getRepos('RafaelHDSV', { params })
+        setRepos(repos)
+      } catch (err) {
+        console.error('Error fetching user repos from GitHub:', err)
+        setError(err as string)
+      } finally {
+        setLoading(false)
+        setError(null)
+      }
+    }
+
+    getReposApi()
+  }, [])
+
+  return {
+    repos,
     loading,
     error
   }
