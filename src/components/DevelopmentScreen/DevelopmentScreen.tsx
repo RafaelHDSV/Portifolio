46ccbb53389eb80c97bf35f3bb8523770dca5847
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-07-04 21:40:55 -0300

    refactor: update DevelopmentScreen component to improve state management and enhance user interaction

--- src/components/DevelopmentScreen/DevelopmentScreen.tsx
+++ src/components/DevelopmentScreen/DevelopmentScreen.tsx
@@ -1,21 +1,48 @@
 import styles from './DevelopmentScreen.module.scss'
 
 interface IDevelopmentScreenProps {
-  clickCount: (value: (prev: number) => number) => void
+  devClickCount: number
+  setDevClickCount: (count: number | ((prev: number) => number)) => void
 }
 
-export default function DevelopmentScreen({ clickCount }: IDevelopmentScreenProps) {
+export default function DevelopmentScreen({
+  devClickCount,
+  setDevClickCount
+}: IDevelopmentScreenProps) {
+  const isUnlocked = devClickCount >= 29
+
+  const handleClick = () => setDevClickCount((prev) => prev + 1)
+
+  const headlineText = isUnlocked ? '' : 'Obrigado por visitar meu '
+  const clickedText = isUnlocked ? 'Parabéns!' : 'portfólio'
+
   return (
     <main className={styles.main}>
       <div className={styles.errorPage}>
-        <img className={styles.leftAstronaut} src='./astronaut.png' alt='astronaut.png' />
-        <img className={styles.rightAstronaut} src='./astronaut2.svg' alt='astronaut2.svg' />
+        <img
+          className={styles.leftAstronaut}
+          src='./astronaut.png'
+          alt='astronaut.png'
+        />
+        <img
+          className={styles.rightAstronaut}
+          src='./astronaut2.svg'
+          alt='astronaut2.svg'
+        />
 
         <div className={styles.errorText}>
           <h1>
-            Obrigado por visitar meu <b onClick={() => clickCount(prev => prev + 1)}>portfólio</b>!
+            {headlineText}
+            <b className={styles.devClickButton} onClick={handleClick}>
+              {clickedText}
+            </b>
           </h1>
-          <p>Este projeto ainda está em desenvolvimento. Fique atento para novidades em breve!</p>
+
+          <p>
+            {isUnlocked
+              ? 'Você desbloqueou o modo de desenvolvimento!'
+              : 'Este projeto ainda está em desenvolvimento. Fique atento para novidades em breve!'}
+          </p>
         </div>
       </div>
 
