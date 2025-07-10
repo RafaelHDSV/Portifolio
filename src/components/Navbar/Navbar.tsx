1ef79fc0b7823cc66a930a80854765d7973134b2
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-07-09 22:38:14 -0300

    feat: add Navbar component with styling for improved navigation

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import { Fade } from 'react-awesome-reveal'
+import styles from './Navbar.module.scss'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Fade damping={0.3}>
+        <a href='/'>
+          <h2>RV</h2>
+        </a>
+      </Fade>
+
+      <div className={styles.optionsNav}>
+        <Fade cascade damping={0.3}>
+          <a href='#about'>Sobre</a>
+          <a href='#languages'>Linguagens</a>
+          <a href='#projects'>Projetos</a>
+          <a href='#contact'>Contato</a>
+        </Fade>
+      </div>
+    </nav>
+  )
+}
