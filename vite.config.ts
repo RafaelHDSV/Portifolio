6c430a3c5388dbefed85090af31eff298ed198a6
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-07-18 22:55:36 -0300

    fix: update SCSS import path to use alias for better readability

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src') // 👈 cria o alias @ para src/
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "src/styles/variables.scss" as *;`
+        additionalData: `@use "@/styles/variables.scss" as *;`
       }
     }
   }
