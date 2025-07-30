10bdf7d248cd51b0567ed7e98fe3c142e5acb3ff
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-07-29 23:10:04 -0300

    refactor: simplify theme initialization and toggle logic in ThemeSwitcher component

--- src/components/SwitchTheme/SwitchTheme.tsx
+++ src/components/SwitchTheme/SwitchTheme.tsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from 'react'
 import styles from './SwitchTheme.module.scss'
 
 export default function ThemeSwitcher() {
-  const storedTheme = localStorage.getItem('theme')
-  const initialTheme = storedTheme ? storedTheme === 'dark' : true
-  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(initialTheme)
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(getInitialTheme)
 
   useEffect(() => {
     if (isDarkTheme) {
@@ -16,14 +14,20 @@ export default function ThemeSwitcher() {
     }
   }, [isDarkTheme])
 
-  function toggleTheme() {
-    setIsDarkTheme((prev) => !prev)
-  }
-
   return (
     <label className={styles.switch}>
-      <input type='checkbox' checked={isDarkTheme} onChange={toggleTheme} />
+      <input
+        type='checkbox'
+        checked={isDarkTheme}
+        onChange={() => setIsDarkTheme((prev) => !prev)}
+      />
       <span className={styles.slider}></span>
     </label>
   )
+}
+
+const getInitialTheme = (): boolean => {
+  const storedTheme = localStorage.getItem('theme')
+  if (storedTheme !== null) return storedTheme === 'dark'
+  return window.matchMedia('(prefers-color-scheme: dark)').matches
 }
