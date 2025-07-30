83fcba2e45c38c8e8fe8bb3774e0f75e9cdd8974
Rafael Vieira <rafaelvieira1720@gmail.com>
2025-07-29 22:40:17 -0300

    refactor: remove redundant NVM initialization from loadNvmAndNode script

--- scripts/loadNvmAndNode.ps1
+++ scripts/loadNvmAndNode.ps1
@@ -1,5 +1,3 @@
-$env:NVM_DIR="$env:USERPROFILE\.nvm"
-. "$env:NVM_DIR\nvm.ps1"
 if (Test-Path .nvmrc) {
     $NODE_VERSION = Get-Content .nvmrc
     nvm use $NODE_VERSION -ErrorAction SilentlyContinue
