40067ce6370c0a61a6e6f3b0d62b4f4c60150fd3
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-05-23 18:55:53 -0300

    refactor: simplify VieiraAnalytics integration by using static project key

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-const ANALYTICS_KEY = import.meta.env.VITE_VIEIRA_ANALYTICS_KEY
-const ANALYTICS_HOST = import.meta.env.VITE_VIEIRA_ANALYTICS_HOST
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    {ANALYTICS_KEY && ANALYTICS_HOST && (
-      <VieiraAnalytics
-        projectKey={ANALYTICS_KEY}
-        endpoint={ANALYTICS_HOST}
-        debug={import.meta.env.DEV}
-      />
-    )}
+    <VieiraAnalytics projectKey="portifolio" />
   </React.StrictMode>
 )
