666067b851cd79a6bb457b431c1363905e1ac1e9
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-05-23 04:20:05 -0300

    feat: integrate VieiraAnalytics for project tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { VieiraAnalytics } from '@vieira/analytics/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -5,5 +6,10 @@ import App from './App'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+
+    <VieiraAnalytics
+      projectKey='portifolio'
+      endpoint='http://localhost:3693/v1'
+    />
   </React.StrictMode>
 )
